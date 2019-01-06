d59bdbaa2b73370ce5165e804831e275fcd043df
Rustam Mehmandarov <mehmandarov@gmail.com>
2019-01-06 17:50:26 +0100

    Fixed merging av two datasets

--- src/main/java/com/mehmandarov/beam/OsloCityBike.java
+++ src/main/java/com/mehmandarov/beam/OsloCityBike.java
@@ -18,7 +18,6 @@ import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.TupleTag;
-import sun.awt.image.ImageWatched;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -43,7 +42,7 @@ public class OsloCityBike {
 
                     stationDataItem.put("availability_bikes", ((LinkedHashMap) stationDataItem.get("availability")).get("bikes"));
                     stationDataItem.put("availability_locks", ((LinkedHashMap) stationDataItem.get("availability")).get("locks"));
-                    stationDataItem.put("availability_overflow_capacity", stationDataItem.get("overflow_capacity"));
+                    stationDataItem.put("availability_overflow_capacity", ((LinkedHashMap) stationDataItem.get("availability")).get("overflow_capacity"));
                     stationDataItem.remove("availability");
                     stationDataItem.put("updated_at", map.get("updated_at"));
 
@@ -100,13 +99,19 @@ public class OsloCityBike {
     }
 
     /** A SimpleFunction that converts station data into an object that can be stored in BigQuery. */
-    static class FormatStationAvailabilityDataFn extends SimpleFunction<KV<Integer, LinkedHashMap>, TableRow>{
+    static class FormatMergedStationDataToBigQueryFn extends SimpleFunction<KV<Integer, LinkedHashMap>, TableRow>{
 
         @Override
         public TableRow apply(KV<Integer, LinkedHashMap> stationAvailability) {
 
             TableRow row = new TableRow()
                     .set("id", stationAvailability.getValue().get("id"))
+                    .set("in_service", stationAvailability.getValue().get("in_service"))
+                    .set("title", stationAvailability.getValue().get("title"))
+                    .set("subtitle", stationAvailability.getValue().get("subtitle"))
+                    .set("number_of_locks", stationAvailability.getValue().get("number_of_locks"))
+                    .set("station_center_lat", stationAvailability.getValue().get("station_center_lat"))
+                    .set("station_center_lon", stationAvailability.getValue().get("station_center_lon"))
                     .set("availability_bikes", stationAvailability.getValue().get("availability_bikes"))
                     .set("availability_locks", stationAvailability.getValue().get("availability_locks"))
                     .set("availability_overflow_capacity", stationAvailability.getValue().get("availability_overflow_capacity"))
@@ -118,6 +123,12 @@ public class OsloCityBike {
         static TableSchema getSchema() {
             List<TableFieldSchema> fields = new ArrayList<>();
             fields.add(new TableFieldSchema().setName("id").setType("INTEGER"));
+            fields.add(new TableFieldSchema().setName("in_service").setType("BOOL"));
+            fields.add(new TableFieldSchema().setName("title").setType("STRING"));
+            fields.add(new TableFieldSchema().setName("subtitle").setType("STRING"));
+            fields.add(new TableFieldSchema().setName("number_of_locks").setType("INTEGER"));
+            fields.add(new TableFieldSchema().setName("station_center_lat").setType("STRING"));
+            fields.add(new TableFieldSchema().setName("station_center_lon").setType("STRING"));
             fields.add(new TableFieldSchema().setName("availability_bikes").setType("INTEGER"));
             fields.add(new TableFieldSchema().setName("availability_locks").setType("INTEGER"));
             fields.add(new TableFieldSchema().setName("availability_overflow_capacity").setType("BOOL"));
@@ -201,36 +212,46 @@ public class OsloCityBike {
         void setOutputFormat(String value);
 
         /**
-         * Set this required option to specify where to write the output.
+         * Set this required option to specify where to write the output for station availability data.
          */
-        @Description("Path of the file to write to")
+        @Description("Path of the file containing station availability data")
         @Default.String("citybikes-stations-availability")
         @Validation.Required
         String getStationOutput();
         void setStationOutput(String value);
 
         /**
-         * Set this required option to specify where to write the output.
+         * Set this required option to specify where to write the output for station metadata.
          */
-        @Description("Path of the file to write to")
+        @Description("Path of the file containing station metadata")
         @Default.String("citybikes-stations-metadata")
         @Validation.Required
         String getMetadataOutput();
         void setMetadataOutput(String value);
 
         /**
-         * Set this required option to specify where to write the output.
+         * Set this required option to specify where to write the output for
+         * joined station metadata and availability data.
          */
-        @Description("Output table name")
-        @Default.String("StationAvailability")
+        @Description("Path of the file containing joined station metadata and availability data")
+        @Default.String("citybikes-stations-and-availability-data")
+        @Validation.Required
+        String getJoinedMetadataAndAvailabilityOutput();
+        void setJoinedMetadataAndAvailabilityOutput(String value);
+
+        /**
+         * Set this required option to specify table name for joined station metadata and availability data in BigQuery.
+         */
+        @Description("Output table name in BigQuery")
+        @Default.String("StationDataAndAvailability")
         @Validation.Required
         String getOutputTableName();
         void setOutputTableName(String value);
 
         /**
-         * Set this required option to specify where to write the output.
+         * Set this required option to specify table name for availability data in BigQuery.
          */
-        @Description("Data set name")
+        @Description("Dataset name")
         @Default.String("OsloCityBike")
         @Validation.Required
         String getOutputDataset();
@@ -256,14 +277,54 @@ public class OsloCityBike {
                 .apply(new StationAvailabilityData());
 
 
-        final TupleTag<LinkedHashMap> metadataIdTag = new TupleTag<LinkedHashMap>();
-        final TupleTag<LinkedHashMap> availabilityIdTag = new TupleTag<LinkedHashMap>();
+        final TupleTag<LinkedHashMap> metadataIdTag = new TupleTag<LinkedHashMap>(){};
+        final TupleTag<LinkedHashMap> availabilityIdTag = new TupleTag<LinkedHashMap>(){};
 
         // Merge collection values into a CoGbkResult collection.
         PCollection<KV<Integer, CoGbkResult>> joinedCollection = KeyedPCollectionTuple.of(metadataIdTag, stationMetadata)
                                                                     .and(availabilityIdTag, availabilityData)
                                                                     .apply(CoGroupByKey.<Integer>create());
 
+        // Process final results - each element in joinedCollection contains two lists.
+        // We need to merge these two lists together.
+        PCollection<LinkedHashMap> finalResultCollection =
+                joinedCollection.apply(ParDo.of(
+                        new  DoFn<KV<Integer, CoGbkResult>, LinkedHashMap>() {
+                            @ProcessElement
+                            public void processElement(ProcessContext c) {
+                                LinkedHashMap<String, String> mergedMap = initMap();
+                                KV<Integer, CoGbkResult> e = c.element();
+
+                                // Return an empty map if no elements with this tag
+                                LinkedHashMap pt1Val = e.getValue().getOnly(availabilityIdTag,
+                                                                            new LinkedHashMap<String, String>());
+                                LinkedHashMap pt2Val = e.getValue().getOnly(metadataIdTag,
+                                                                            new LinkedHashMap<String, String>());
+
+                                mergedMap.putAll(pt1Val);
+                                mergedMap.putAll(pt2Val);
+                                c.output(mergedMap);
+                            }
+
+                            // Should be replaced with a proper object.
+                            // Ok(-ish) for a demo, not OK in production code... :-)
+                            private LinkedHashMap<String, String> initMap(){
+                                LinkedHashMap<String,String> myMap = new LinkedHashMap<>();
+                                myMap.put("id", "");
+                                myMap.put("in_service", "");
+                                myMap.put("title", "");
+                                myMap.put("subtitle", "");
+                                myMap.put("number_of_locks", "");
+                                myMap.put("station_center_lat", "");
+                                myMap.put("station_center_lon", "");
+                                myMap.put("availability_bikes", "");
+                                myMap.put("availability_locks", "");
+                                myMap.put("availability_overflow_capacity", "");
+                                myMap.put("updated_at", "");
+                                return myMap;
+                            }
+                        }
+                ));
 
         if (options.getOutputFormat().equalsIgnoreCase("bq")) {
             // Needed for BigQuery
@@ -272,21 +333,24 @@ public class OsloCityBike {
             tableRef.setProjectId(options.as(GcpOptions.class).getProject());
             tableRef.setTableId(options.getOutputTableName());
 
-            availabilityData.apply(MapElements.via(new FormatStationAvailabilityDataFn()))
+            availabilityData.apply(MapElements.via(new FormatMergedStationDataToBigQueryFn()))
                     .apply(BigQueryIO.writeTableRows().to(tableRef)
-                            .withSchema(FormatStationAvailabilityDataFn.getSchema())
+                            .withSchema(FormatMergedStationDataToBigQueryFn.getSchema())
                             .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
                             .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
                     );
         } else if (options.getOutputFormat().equalsIgnoreCase("files")) {
-            availabilityData.apply(MapElements.via(new FormatAsTextFn()))
-                    .apply("WriteStationData", TextIO.write().to(options.getStationOutput()));
+            //availabilityData.apply(MapElements.via(new FormatAsTextFn()))
+            //        .apply("WriteStationData", TextIO.write().to(options.getStationOutput()));
 
-            stationMetadata.apply(MapElements.via(new FormatAsTextFn()))
-                    .apply("WriteStationMetaData", TextIO.write().to(options.getMetadataOutput()));
+            //stationMetadata.apply(MapElements.via(new FormatAsTextFn()))
+            //        .apply("WriteStationMetaData", TextIO.write().to(options.getMetadataOutput()));
 
-            joinedCollection.apply(MapElements.via(new FormatAnytihngAsTextFn()))
-                    .apply("WriteJoinedData", TextIO.write().to("tmp-JOINED-data.txt"));
+            //joinedCollection.apply(MapElements.via(new FormatAnytihngAsTextFn()))
+            //        .apply("WriteJoinedData", TextIO.write().to("tmp-JOINED-data.txt"));
+
+            finalResultCollection.apply(MapElements.via(new FormatAnytihngAsTextFn()))
+                    .apply("WriteJoinedData", TextIO.write().to(options.getJoinedMetadataAndAvailabilityOutput()));
         }
 
         pipeline.run().waitUntilFinish();
